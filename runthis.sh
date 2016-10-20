b8baa0a468e2b9383bd4ecc43f8772796c591c82
HoneyWell-Quest <vinodm.pes@gmail.com>
2016-10-20 15:09:57 +0530

    missed commad

--- runthis.sh
+++ runthis.sh
@@ -2,6 +2,7 @@
 
 `sudo chmod 777 /etc/rc.local`
 `sudo chmod +x linux.sh`
+`sudo chmod 777 linux.sh`
 `sudo sed -i 's/exit 0//g' /etc/rc.local`
 `sudo readlink -f linux.sh >> /etc/rc.local`
 `sudo echo "exit 0" >> /etc/rc.local`
